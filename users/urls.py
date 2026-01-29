4f921c8cc0c1e167034eb14126e827f82844c0d4
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-01-29 04:15:02 +0000

    Added feature for users to login and signup

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.contrib.auth import urls as auth_urls
+from django.urls import include, path
 
 from users.views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    # Include default auth urls (login, logout, password_change, etc.)
+    path("", include(auth_urls)),
 ]
