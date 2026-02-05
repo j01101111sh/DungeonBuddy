e8d9862bbe94fdf3bde4638adde4fc8f44f81021
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-02-04 23:18:08 -0600

    Feature/user profiles (#54)
    * Added user profile
    
    * Updated to link to profiles
    
    * Enhance user profile detail layout with card design and additional information
    
    * Remove accidental file
    
    * Unit testing for user profiles
    
    * Fixed display bug
    
    * expanded test coverage
    
    * Fixed incorrect bootstrap class
    
    * fixed test logic
    
    * Improved unit test

--- users/views/profiles.py
+++ users/views/profiles.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import QuerySet
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
+from django.views.generic import DetailView, UpdateView
 
 from users.forms import CustomUserChangeForm
 
@@ -27,3 +27,10 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 
         messages.success(self.request, "Your profile has been updated successfully.")
         return super().form_valid(form)
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    template_name = "users/user_detail.html"
