59e9de8763b8637215a7859467f6385f1ff7af47
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-02-10 05:11:23 +0000

    Added type annotations

--- dunbud/views/session_create.py
+++ dunbud/views/session_create.py
@@ -1,6 +1,9 @@
+from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.views.generic import CreateView
 
 from ..forms import SessionForm
@@ -14,21 +17,21 @@ class SessionCreateView(LoginRequiredMixin, CreateView):
     form_class = SessionForm
     template_name = "session/session_form.html"
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         """Redirect to the campaign detail page on success."""
-        return reverse_lazy(
+        return reverse(
             "campaign_detail",
             kwargs={"pk": self.kwargs["campaign_pk"]},
         )
 
-    def form_valid(self, form):
+    def form_valid(self, form: SessionForm) -> HttpResponse:
         """Set the campaign and proposer on the session."""
         campaign = get_object_or_404(Campaign, pk=self.kwargs["campaign_pk"])
         form.instance.campaign = campaign
         form.instance.proposer = self.request.user
         return super().form_valid(form)
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         """Add the campaign to the context."""
         context = super().get_context_data(**kwargs)
         context["campaign"] = get_object_or_404(Campaign, pk=self.kwargs["campaign_pk"])
--- dunbud/views/session_toggle_attendance.py
+++ dunbud/views/session_toggle_attendance.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 
@@ -8,14 +11,20 @@ from ..models import Session
 class SessionToggleAttendanceView(LoginRequiredMixin, View):
     """View for toggling a user's attendance for a session."""
 
-    def post(self, request, *args, **kwargs):
+    def post(
+        self,
+        request: HttpRequest,
+        *args: Any,
+        **kwargs: Any,
+    ) -> HttpResponse:
         """Toggle the user's attendance for the session."""
         session = get_object_or_404(Session, pk=self.kwargs["pk"])
         user = request.user
 
-        if user in session.attendees.all():
-            session.attendees.remove(user)
-        else:
-            session.attendees.add(user)
+        if user.is_authenticated:
+            if user in session.attendees.all():
+                session.attendees.remove(user)
+            else:
+                session.attendees.add(user)
 
         return redirect("campaign_detail", pk=session.campaign.pk)
