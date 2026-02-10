5e2b9150cbd119fedae3286d6fbfb63b40eeb58a
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-02-10 05:43:09 +0000

    Refactor SessionCreateView to store campaign as an instance attribute for improved code clarity

--- dunbud/views/session_create.py
+++ dunbud/views/session_create.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBase
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import CreateView
@@ -17,6 +17,11 @@ class SessionCreateView(LoginRequiredMixin, CreateView):
     form_class = SessionForm
     template_name = "session/session_form.html"
 
+    def dispatch(self, request: Any, *args: Any, **kwargs: Any) -> HttpResponseBase:
+        """Fetch the campaign and store it as an instance attribute."""
+        self.campaign = get_object_or_404(Campaign, pk=self.kwargs["campaign_pk"])
+        return super().dispatch(request, *args, **kwargs)
+
     def get_success_url(self) -> str:
         """Redirect to the campaign detail page on success."""
         return reverse(
@@ -26,13 +31,12 @@ class SessionCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form: SessionForm) -> HttpResponse:
         """Set the campaign and proposer on the session."""
-        campaign = get_object_or_404(Campaign, pk=self.kwargs["campaign_pk"])
-        form.instance.campaign = campaign
+        form.instance.campaign = self.campaign
         form.instance.proposer = self.request.user
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         """Add the campaign to the context."""
         context = super().get_context_data(**kwargs)
-        context["campaign"] = get_object_or_404(Campaign, pk=self.kwargs["campaign_pk"])
+        context["campaign"] = self.campaign
         return context
