0cf146e175d070eedd705c1d59f743fc6404c2d5
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2026-02-12 16:30:35 +0000

    Feature: add new bash aliases for development convenience

--- .devcontainer/bash_aliases.sh
+++ .devcontainer/bash_aliases.sh
@@ -8,4 +8,6 @@ alias djc='dj collectstatic --noinput'
 alias djmm='dj makemigrations'
 alias djm='dj migrate'
 alias ds='./dev_setup.sh'
+alias dsr='ds && djr'
 alias prunelocal="git fetch --prune && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D"
+alias gac='git add . && pcr'
